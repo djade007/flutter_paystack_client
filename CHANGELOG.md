b14585ab8b3d798297749778c7e3bbf47a09cf8f
Olajide Afeez <jide4real43@gmail.com>
2021-03-16 23:09:51 +0100

    Bump version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## [0.1.1]
+
+* Fix web build issue
+
 ## [0.1.0]
 
 * Migrate to null safety
--- lib/src/paystack_web.dart
+++ lib/src/paystack_web.dart
@@ -1,9 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter_paystack/flutter_paystack.dart';
-import 'package:js/js.dart';
 
 import '../flutter_paystack_client.dart';
+import 'js/js_stub.dart' if (dart.library.js) 'package:js/js.dart';
 import 'js/paystack_stub.dart' if (dart.library.js) 'js/paystack_js.dart';
 
 class PaystackWeb {
